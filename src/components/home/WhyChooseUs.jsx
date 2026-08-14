cf325551996a263165b5d2347a018b8e817a4fdd
Zawwar6 <zawwarahmed06@gmail.com>
2026-08-15 00:52:06 +0500

    Gallery pic changes

--- src/components/home/WhyChooseUs.jsx
+++ src/components/home/WhyChooseUs.jsx
@@ -11,7 +11,7 @@ export default function WhyChooseUs() {
         preload="auto"
         className="block w-full h-auto"
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="/video2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </section>
